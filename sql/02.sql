ebd92588b43a846fa95f2e54d5230ed1c7566609
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 13:48:51 -0800

    update

--- sql/02.sql
+++ sql/02.sql
@@ -1,3 +1,9 @@
 /*
  * Compute the country with the most customers in it. 
  */
+
+SELECT country FROM customer 
+JOIN address USING (address_id)
+JOIN city USING (city_id)
+JOIN country USING (country_id)
+GROUP BY country ORDER BY COUNT(customer_id) DESC WHERE ROWNUM == 1;
