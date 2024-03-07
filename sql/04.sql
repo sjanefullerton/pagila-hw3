3f2164ed06da2157f169e77b14a84341905d6524
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 14:07:12 -0800

    update

--- sql/04.sql
+++ sql/04.sql
@@ -3,3 +3,25 @@
  * 1. have appeared in at least one movie in the "Children" category,
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
+
+SELECT first_name, last_name FROM actor
+WHERE actor_id IN (
+    SELECT actor_id
+    FROM actor
+    WHERE film_id IN (
+        SELECT film_id
+        FROM film
+        WHERE category = 'Children'
+    )
+)
+AND actor_id NOT IN (
+    SELECT actor_id
+    FROM actor
+    WHERE film_id IN (
+        SELECT film_id
+        FROM film
+        WHERE category = 'Horror'
+    )
+);
+
+
