ec3e6783643bde8df91d99d394cfb48775fc78cb
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 19:28:48 -0800

    update

--- sql/05f.sql
+++ sql/05f.sql
@@ -8,20 +8,16 @@
  * but I find the version using set operations much more intuitive.
  */
 
-SELECT f.title
-FROM film AS f
-JOIN film_actor AS fa USING (film_id)
-JOIN actor AS a ON fa.actor_id = a.actor_id
-JOIN film_category AS fc USING (film_id)
-JOIN (
-    SELECT film_id
-    FROM film_category
-    JOIN category ON film_category.category_id = category.category_id
-    WHERE category.name = 'AMERICAN CIRCUS'
-) AS ac ON fc.film_id = ac.film_id
-WHERE f.film_id != ac.film_id
-GROUP BY f.title
-HAVING COUNT(DISTINCT fc.category_id) = 2
-   AND COUNT(DISTINCT fa.actor_id) = 1
-ORDER BY f.title;
+SELECT f.title FROM film f
+JOIN film_actor fa USING (film_id)
+JOIN film_actor a ON (fa.actor_id = a.actor_id)
+JOIN film ff ON (a.film_id = ff.film_id)
+WHERE ff.title = 'AMERICAN CIRCUS'
+INTERSECT
+SELECT f.title FROM film f
+JOIN film_category fc USING (film_id)
+JOIN film_category fcc ON (fc.category_id = fcc.category_id)
+JOIN film ff ON (fcc.film_id = ff.film_id) WHERE ff.title = 'AMERICAN CIRCUS'
+GROUP BY f.title 
+HAVING COUNT(DISTINCT f.title) >= 2;
 
