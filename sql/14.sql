325abbedaa99dc0b11f0d1dc98154a49b4fe7ece
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 23:23:39 -0800

    update

--- sql/14.sql
+++ sql/14.sql
@@ -7,3 +7,15 @@
  * Note that in the last query, we were ranking films by the total amount of payments made,
  * but in this query, you are ranking by the total number of times the movie has been rented (and ignoring the price).
  */
+
+SELECT t.name, t.title, t.count AS "total rentals" FROM category c
+JOIN LATERAL (
+    SELECT COUNT(rental_id) AS count, title, name FROM rental
+    JOIN inventory USING (inventory_id)
+    JOIN film USING (film_id)
+    JOIN film_cateogry USING (film_id)
+    JOIN category cat USING (category_id)
+    WHERE category_id = c.category_id
+    GROUP BY title, name 
+    ORDER BY COUNT(rental_id) DESC, title DEC LIMIT 5) t ON TRUE
+ORDER BY name, count DESC, title;
