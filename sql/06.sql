ddf707dcf847063103d1de235689270609309085
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 20:58:27 -0800

    update

--- sql/06.sql
+++ sql/06.sql
@@ -7,15 +7,34 @@
  * Do not list 'RUSSELL BACALL', since he has a Bacall Number of 0.
  */
 
-SELECT first_name || ' ' || last_name AS "Actor Name" FROM actor
-JOIN film_actor USING (actor_id)
-WHERE film_id IN (
-    SELECT film_id FROM film_actor
-    JOIN actor USING (actor_id)
-    WHERE first_name = 'RUSSELL' AND last_name = 'BACALL'
-) AND NOT (first_name = 'RUSSELL' AND last_name = 'BACALL')
+SELECT DISTINCT a1.first_name || ' ' || a1.last_name AS "Actor Name"
+FROM actor a1
+JOIN film_actor fa1 ON a1.actor_id = fa1.actor_id
+JOIN film_actor fa2 ON fa1.film_id = fa2.film_id
+JOIN actor a2 ON fa2.actor_id = a2.actor_id
+WHERE a2.actor_id IN (
+        SELECT actor_id FROM actor
+        WHERE first_name = 'RUSSELL' and last_name = 'BACALL'
+    )
+AND a1.actor_id NOT IN (
+        SELECT actor_id FROM actor
+        WHERE first_name = 'RUSSELL' and last_name = 'BACALL'
+    )
+AND a1.actor_id NOT IN (
+        SELECT a3.actor_id FROM actor a3
+        JOIN film_actor fa3 ON a3.actor_id = fa3.actor_id
+        JOIN film_actor fa4 ON fa3.film_id = fa4.film_id
+        JOIN actor a4 ON fa4.actor_id = a4.actor_id
+        WHERE a4.actor_id IN (
+                SELECT actor_id FROM actor
+                WHERE first_name = 'RUSSELL' and last_name = 'BACALL'
+            )
+    )
+AND a1.actor_id NOT IN (
+        SELECT actor_id FROM actor
+        WHERE first_name = 'RUSSELL' and last_name = 'BACALL'
+    )
 ORDER BY "Actor Name";
-
 
 
 
