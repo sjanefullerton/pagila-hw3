587cc4c6a54f8b190cd952dfc7323a8788da0201
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 23:17:56 -0800

    update

--- sql/13.sql
+++ sql/13.sql
@@ -9,3 +9,16 @@
  * For correct output, you will have to rank the films for each actor.
  * My solution uses the `rank` window function.
  */
+
+select * FROM (SELECT actor_id, first_name, last_name, film_id, title,
+    RANK() OVER (PARTITION BY actor_id ORDER BY SUM(amount) DESC, film_id) AS rank,
+    SUM(amount) AS revenue FROM actor act
+    JOIN film_actor filma USING (actor_id)
+    JOIN film USING (film_id)
+    JOIN inventory USING (film_id)
+    JOIN rental USING (inventory_id)
+    JOIN payment USING (rental_id)
+    GROUP BY 1,2,3,4,5
+    ORDER BY 1,6) t
+where rank <= 3;
+
