fad475ee4962b9e3aad3dfbab43449f0425f4546
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 20:01:43 -0800

    update

--- sql/06.sql
+++ sql/06.sql
@@ -6,3 +6,17 @@
  * That is, list all actors that have appeared in a film with 'RUSSELL BACALL'.
  * Do not list 'RUSSELL BACALL', since he has a Bacall Number of 0.
  */
+
+SELECT first_name || ' ' || last_name AS "Actor Name" FROM actor
+JOIN film_actor USING (actor_id)
+WHERE film_id IN (
+    SELECT film_id FROM film_actor
+    JOIN actor USING (actor_id)
+    WHERE first_name = 'RUSSELL' AND last_name = 'BACALL'
+) AND NOT (first_name = 'RUSSELL' AND last_name = 'BACALL')
+ORDER BY "Actor Name";
+
+
+
+
+
