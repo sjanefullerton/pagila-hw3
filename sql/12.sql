aceb9b54bf52c7a2c493d568ff45311e039dfd16
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 22:06:08 -0800

    update

--- sql/12.sql
+++ sql/12.sql
@@ -4,3 +4,17 @@
  *
  * Write a SQL query that finds all action fanatics.
  */
+
+SELECT cmer.customer_id, cmer.first_name, cmer.last_name FROM customer cmer
+LEFT JOIN lateral (
+    SELECT rental_id, rental_date, inventory_id, name FROM rental
+    JOIN inventory USING (inventory_id)
+    JOIN film_category USING (film_id)
+    JOIN category cat USING (category_id)
+    WHERE customer_Id = cmer.customer_id
+    ORDER BY rental_date DESC ) AS t ON true
+WHERE t.name = 'Action'
+GROUP BY cmer.customer_id
+HAVING COUNT(cmer.customer_id) >= 4;
+
+
