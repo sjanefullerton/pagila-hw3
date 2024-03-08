0d711320b1c921f479d30e6d74c0061ecc0a78ea
Sarah Fullerton <sjanesasha@gmail.com>
2024-03-07 23:28:57 -0800

    update

--- sql/15.sql
+++ sql/15.sql
@@ -8,3 +8,15 @@
  * But there is also a view in the database that contains the correct formatting,
  * and you can SELECT from that VIEW instead of constructing the entire query manually.
  */
+
+SELECT title, STRING_AGG(
+    INITCAP(first_name) || INITCAP(last_name), ', ') AS actors FROM (
+    SELECT title,
+    unnest(string_to_array(actors, ', ')) AS actors FROM film_list
+    WHERE rating = 'G'
+    AND category = 'Documentary') stragg
+GROUP BY title
+ORDER BY title;
+
+
+
